295518b3023e5d0c8c80d20130c14c75f0d7e9b8
Luca Herlein <luca@herlein.com>
2024-07-21 22:55:46 -0700

    added mpu and bmp sensor support

--- main.py
+++ main.py
@@ -1,44 +1,90 @@
-from machine import Pin
+from machine import Pin, I2C
+from bmp180 import BMP180
+from mpu6050 import MPU6050
 import time
-import network   # handles connecting to WiFi
-import urequests # handles making and servicing network requests
+import math
+
+def calibrateSensors():
+  sealevel = 0
+  phiNormal = 0
+  thetaNormal = 0
+  # Average N readings
+  N = 100
+  for i in range(N):
+    sealevel += bmp.pressure
+    imuData = readIMU()
+    orientation = calcAngles(imuData)
+    phiNormal += orientation[0]
+    thetaNormal += orientation[1]
+    time.sleep_ms(10)
+
+  bmp.sealevel = sealevel/N
+  return [phiNormal/N, thetaNormal/N, sealevel/N]
+
+def readIMU():
+  # Get IMU Data
+  accelData = mpu.read_accel_data()
+  gyroData = mpu.read_gyro_data()
+
+  return [accelData, gyroData]
+
+def readBMP():
+  # Get BMP Data
+  pressure = bmp.pressure
+  altitude = bmp.altitude
+  temperature = bmp.temperature
+
+  return [pressure, altitude, temperature]
+
+def calcAngles(imuData): # Not entirely sure this will work when in flight
+
+  # Get Accelerometer Data
+  accelData = imuData[0]
+
+  phi = math.atan2(accelData[1], accelData[2])
+  theta = math.atan2(accelData[0], accelData[2])
+
+  return [phi, theta]
 
 print("Starting...")
 
-# Fill in your network name (ssid) and password here:
-ssid = ''
-password = ''
+# Define Pins
+mpuSDA = Pin(14) # i2c 1
+mpuSCL = Pin(15)
 
-def connect():
-  #Connect to WLAN
-  wlan = network.WLAN(network.STA_IF)
-  wlan.active(True)
-  wlan.connect(ssid, password)
-  while wlan.isconnected() == False:
-    print('Waiting for connection...')
-    sleep(1)
-  print(wlan.ifconfig())
+bmpSDA = Pin(16) # i2c 0
+bmpSCL = Pin(17)
 
-try:
-  print("Connecting to network...")
-  connect()
-  print("Connected to network")
-except Exception as e:
-  print("Error connecting to network: ", e)
-  break
+# Define I2C Busses
+i2c_mpu = I2C(1, sda=mpuSDA, scl=mpuSCL)
+i2c_bmp = I2C(0, sda=bmpSDA, scl=bmpSCL)
 
-# Example 1. Make a GET request for google.com and print HTML
-# Print the html content from google.com
-print("1. Querying google.com:")
-r = urequests.get("http://www.google.com")
-print(r.content)
+# Define Sensors
+bmp = BMP180(i2c_bmp)
+mpu = MPU6050(i2c_mpu)
 
+# Calibrate Everything
+print("Calibrating Sensors")
+normalValues = calibrateSensors()
 
-led = Pin(25, Pin.OUT)
+# Wake up MPU
+print("Waking up MPU")
+mpu.wake()
 
 count = 0
 while True:
-  led.toggle()
-  print("Hello, World! ", count)
-  count += 1
-  time.sleep(0.5)
+  # Read IMU Data
+  imuData = readIMU()
+
+  # Read BMP Data
+  bmpData = readBMP()
+
+  # Calculate Angles
+  angles = calcAngles(imuData)
+
+  # Print Data
+  print("IMU Data: ", imuData)
+  print("BMP Data: ", bmpData)
+  print("Angles: ", angles)
+
+  time.sleep(.5)
