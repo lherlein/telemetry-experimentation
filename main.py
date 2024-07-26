26c1be61bc3c5a0b578abd67080601117b196692
Luca Herlein <luca@herlein.com>
2024-07-26 11:06:46 -0700

    added controller lib and updated main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, PWM, UART
 from bmp180 import BMP180
 from mpu6050 import MPU6050
+from controller import PID, PI
 import time
 import math
+import machine
+import _thread
 
 def calibrateSensors():
   sealevel = 0
@@ -46,6 +49,45 @@ def calcAngles(imuData): # Not entirely sure this will work when in flight
 
   return [phi, theta]
 
+def rad2deg(rad):
+  return rad * 180 / math.pi
+
+def deg2rad(deg):
+  return deg * math.pi / 180
+
+def ms2ns(ms):
+  return ms * 1000000
+
+def pos2u16(pos):
+  # get value between 0 and 100 representing servo position
+  # return value between 0 and 65535 representing duty cycle between 0 and 100%
+  # MUST KEEP RETURN BETWEEN 5-10% DUTY CYCLE
+
+  # 0 input -> 65535*0.05
+  # 100 input -> 65535*0.1
+
+  posPercent = pos / 100
+
+  # convert posPercent to duty cycle within bounds
+  duty = 0.05 + posPercent * 0.05
+
+  return int(duty * 65535)
+
+#Function to handle reading from the uart serial to a buffer
+def SerialRead(mode):
+  SerialRecv = ""
+  if mode == "0" :
+    SerialRecv=str(uart.readline())
+  else:
+    SerialRecv=str(uart.read(mode))
+  #replace generates less errors than .decode("utf-8")
+  SerialRecv=SerialRecv.replace("b'", "")
+  SerialRecv=SerialRecv.replace("\\r", "")
+  SerialRecv=SerialRecv.replace("\\n", "\n")
+  SerialRecv=SerialRecv.replace("'", "")
+  return SerialRecv
+
+
 print("Starting...")
 
 # Define Pins
@@ -54,6 +96,9 @@ mpuSCL = Pin(15)
 
 bmpSDA = Pin(16) # i2c 0
 bmpSCL = Pin(17)
+
+wifiRx = Pin(0)
+wifiTx = Pin(1)
 
 # Define I2C Busses
 i2c_mpu = I2C(1, sda=mpuSDA, scl=mpuSCL)
@@ -71,7 +116,41 @@ normalValues = calibrateSensors()
 print("Waking up MPU")
 mpu.wake()
 
-count = 0
+# Start Webserver
+print("Starting Webserver")
+#Set variables
+recv=""
+recv_buf="" 
+# wifi credentials (if needed)
+wifi_ssid = ("sofia")
+wifi_password = ("19631964")
+
+print ("Setting up Webserver...")
+uart = UART(0,115200, tx=Pin(0), rx=Pin(1)) # uart on uart1 with baud of 115200
+time.sleep(2)
+# Connect to wifi, this only needs to run once, ESP will retain the CWMODE and wifi details and reconnect after power cycle, leave commented out unless this has been run once.
+print ("  - Setting AP Mode...")
+uart.write('AT+CWMODE=1'+'\r\n')
+time.sleep(2)
+print ("  - Connecting to WiFi...")
+uart.write('AT+CWJAP="'+wifi_ssid+'","'+wifi_password+'"'+'\r\n')
+time.sleep(5)
+print ("  - Setting Connection Mode...")
+uart.write('AT+CIPMUX=1'+'\r\n')
+time.sleep(2)
+print ("  - Starting Webserver..")
+uart.write('AT+CIPSERVER=1,80'+'\r\n') #Start webserver on port 80
+time.sleep(2)
+# Get local IP and print result
+uart.write('AT+CIFSR'+'\r\n')
+time.sleep(0.5)
+ipRes = uart.read()
+print(ipRes)
+
+print ("Webserver Ready!")
+print("")
+
+# Main Loop
 while True:
   # Read IMU Data
   imuData = readIMU()
@@ -81,10 +160,24 @@ while True:
 
   # Calculate Angles
   angles = calcAngles(imuData)
+  angles = [rad2deg(angles[0]), rad2deg(angles[1])]
 
-  # Print Data
-  print("IMU Data: ", imuData)
-  print("BMP Data: ", bmpData)
-  print("Angles: ", angles)
+  # Craft Data String
+  data = "Angles: " + str(angles) + "\n" + "Pressure: " + str(bmpData[0]) + "\n" + "Altitude: " + str(bmpData[1]) + "\n" + "Temperature: " + str(bmpData[2]) + "\n"
 
-  time.sleep(.5)
+  print(data)
+
+  # Get Data Length
+  dataLength = len(data)
+
+  uart.write('AT+CIPSEND=0,'+str(dataLength)+'\r\n')
+  time.sleep(0.1)
+  uart.write(data)
+
+  while True:
+    #read a byte from serial into the buffer
+    recv=SerialRead(1)
+    recv_buf=recv_buf+recv
+
+    print(recv)
+    time.sleep(0.1)
